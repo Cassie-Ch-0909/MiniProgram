ab94cb0d42d806bc7282972bfe8dcbf53077c113
Cassie-Ch-0909 <cassie20190909@gmail.com>
2024-04-17 20:51:25 +0800

    自定义首页导航栏以及引入iconfont

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,11 +1,14 @@
 // pages/home/home.js
+let app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    navBarHeight: app.globalData.navBarHeight,//导航栏高度
+    menuBotton: app.globalData.menuBotton,//导航栏距离顶部距离
+    menuHeight: app.globalData.menuHeight //导航栏高度
   },
 
   /**
