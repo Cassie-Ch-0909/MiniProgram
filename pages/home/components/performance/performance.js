16ec18146150b786bf0c88581664de39b03e9cb3
Cassie-Ch-0909 <cassie20190909@gmail.com>
2024-04-19 18:57:21 +0800

    演出日历、演出攻略、拼团领券模块UI布局完成

---
+++ pages/home/components/performance/performance.js
@@ -0,0 +1,28 @@
+// pages/home/components/performance/performance.js
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: String,
+    content: String,
+    img:String,
+    isHidden:Boolean,
+    isBorder:Boolean,
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
