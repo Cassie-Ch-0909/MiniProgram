b5c6f2c7c0d56f6a98404cbac4429354d09c27da
Cassie-Ch-0909 <cassie20190909@gmail.com>
2024-04-21 12:57:59 +0800

    配置goodModule分包，新建搜索页面，点击首页搜索框可跳转到搜索页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -20,20 +20,20 @@ Page({
     // 今日秒杀数组
     imageList: [
       {
-        img:"https://gw.alicdn.com/bao/uploaded/i3/2251059038/O1CN01tcNq1I2GdSaXaCVsI_!!0-item_pic.jpg_q60.jpg",
-        type:"演唱会"
+        img: "https://gw.alicdn.com/bao/uploaded/i3/2251059038/O1CN01tcNq1I2GdSaXaCVsI_!!0-item_pic.jpg_q60.jpg",
+        type: "演唱会"
       },
       {
-        img:"https://gw.alicdn.com/bao/uploaded/https:img.alicd…/O1CN012K2mOx2GdSaEwon0z_!!2251059038.jpg_q60.jpg",
-        type:"音乐节"
+        img: "https://gw.alicdn.com/bao/uploaded/https:img.alicd…/O1CN012K2mOx2GdSaEwon0z_!!2251059038.jpg_q60.jpg",
+        type: "音乐节"
       },
       {
-        img:"https://gw.alicdn.com/bao/uploaded/i3/2251059038/O1CN01fNGpi12GdSaZkAQPd_!!0-item_pic.jpg_q60.jpg",
-        type:"音乐节"
+        img: "https://gw.alicdn.com/bao/uploaded/i3/2251059038/O1CN01fNGpi12GdSaZkAQPd_!!0-item_pic.jpg_q60.jpg",
+        type: "音乐节"
       },
     ],
     // 扁平轮播图数组
-    swiperList:[
+    swiperList: [
       "https://gw.alicdn.com/imgextra/i4/O1CN01fqjSXd1GaFbTrTk4A_!!6000000000638-0-tps-1404-320.jpg_Q30.jpg",
       "https://gw.alicdn.com/imgextra/i4/O1CN01YdrGu21ehcn37DlV3_!!6000000003903-0-tps-1404-320.jpg_Q30.jpg",
       "https://gw.alicdn.com/imgextra/i4/O1CN01fqjSXd1GaFbTrTk4A_!!6000000000638-0-tps-1404-320.jpg_Q30.jpg",
@@ -42,66 +42,66 @@ Page({
       "https://gw.alicdn.com/imgextra/i4/O1CN01LyqLy61FocRfBnh3I_!!6000000000534-0-tps-1404-320.jpg_Q30.jpg",
     ],
     // 本周演出scroll-x的数组
-    thisWeekScrollXList:[
+    thisWeekScrollXList: [
       {
-        img:"https://img.alicdn.com/bao/uploaded/i1/2251059038/O1CN01SwGBKy2GdSaQ78f78_!!0-item_pic.jpg",
-        info:"棱镜「多少年·多少面」2024巡回演唱会—北京站",
-        position:"华熙LIVE·五棵松",
-        price:"380"
+        img: "https://img.alicdn.com/bao/uploaded/i1/2251059038/O1CN01SwGBKy2GdSaQ78f78_!!0-item_pic.jpg",
+        info: "棱镜「多少年·多少面」2024巡回演唱会—北京站",
+        position: "华熙LIVE·五棵松",
+        price: "380"
       },
       {
-        img:"https://img.alicdn.com/bao/uploaded/https://img.al…51059038/O1CN01aZ35xy2GdSabqhDh8_!!2251059038.jpg",
-        info:"2024北京无限音乐节",
-        position:"北京通州区城市绿心森林公园南门(绿创空间对面)",
-        price:"380"
+        img: "https://img.alicdn.com/bao/uploaded/https://img.al…51059038/O1CN01aZ35xy2GdSabqhDh8_!!2251059038.jpg",
+        info: "2024北京无限音乐节",
+        position: "北京通州区城市绿心森林公园南门(绿创空间对面)",
+        price: "380"
       },
       {
-        img:"https://img.alicdn.com/bao/uploaded/https://img.al…51059038/O1CN01hFwRF32GdSaOCEPTm_!!2251059038.png",
-        info:"苏有朋在多重宇宙中遇见你演唱会-北京站",
-        position:"首都体育馆",
-        price:"380"
+        img: "https://img.alicdn.com/bao/uploaded/https://img.al…51059038/O1CN01hFwRF32GdSaOCEPTm_!!2251059038.png",
+        info: "苏有朋在多重宇宙中遇见你演唱会-北京站",
+        position: "首都体育馆",
+        price: "380"
       },
       {
-        img:"https://img.alicdn.com/bao/uploaded/https://img.al…51059038/O1CN01MV6J3g2GdSZ4qiOyn_!!2251059038.png",
-        info:"2024中超联赛天津津门虎主场赛事（年票）",
-        position:"天津泰达足球场",
-        price:"680"
+        img: "https://img.alicdn.com/bao/uploaded/https://img.al…51059038/O1CN01MV6J3g2GdSZ4qiOyn_!!2251059038.png",
+        info: "2024中超联赛天津津门虎主场赛事（年票）",
+        position: "天津泰达足球场",
+        price: "680"
       },
       {
-        img:"https://img.alicdn.com/bao/uploaded/i3/2251059038/O1CN01FT1QXb2GdSZujlBWG_!!2-item_pic.png",
-        info:"2023-2024WCBA 山西竹叶青女篮季后赛（套票）",
-        position:"山西体育中心体育馆",
-        price:"179"
+        img: "https://img.alicdn.com/bao/uploaded/i3/2251059038/O1CN01FT1QXb2GdSZujlBWG_!!2-item_pic.png",
+        info: "2023-2024WCBA 山西竹叶青女篮季后赛（套票）",
+        position: "山西体育中心体育馆",
+        price: "179"
       },
       {
-        img:"https://img.alicdn.com/bao/uploaded/i1/2251059038/O1CN01SwGBKy2GdSaQ78f78_!!0-item_pic.jpg",
-        info:"棱镜「多少年·多少面」2024巡回演唱会—北京站",
-        position:"华熙LIVE·五棵松",
-        price:"380"
+        img: "https://img.alicdn.com/bao/uploaded/i1/2251059038/O1CN01SwGBKy2GdSaQ78f78_!!0-item_pic.jpg",
+        info: "棱镜「多少年·多少面」2024巡回演唱会—北京站",
+        position: "华熙LIVE·五棵松",
+        price: "380"
       },
       {
-        img:"https://img.alicdn.com/bao/uploaded/https://img.al…51059038/O1CN01aZ35xy2GdSabqhDh8_!!2251059038.jpg",
-        info:"2024北京无限音乐节",
-        position:"北京通州区城市绿心森林公园南门(绿创空间对面)",
-        price:"380"
+        img: "https://img.alicdn.com/bao/uploaded/https://img.al…51059038/O1CN01aZ35xy2GdSabqhDh8_!!2251059038.jpg",
+        info: "2024北京无限音乐节",
+        position: "北京通州区城市绿心森林公园南门(绿创空间对面)",
+        price: "380"
       },
       {
-        img:"https://img.alicdn.com/bao/uploaded/https://img.al…51059038/O1CN01hFwRF32GdSaOCEPTm_!!2251059038.png",
-        info:"苏有朋在多重宇宙中遇见你演唱会-北京站",
-        position:"首都体育馆",
-        price:"380"
+        img: "https://img.alicdn.com/bao/uploaded/https://img.al…51059038/O1CN01hFwRF32GdSaOCEPTm_!!2251059038.png",
+        info: "苏有朋在多重宇宙中遇见你演唱会-北京站",
+        position: "首都体育馆",
+        price: "380"
       },
       {
-        img:"https://img.alicdn.com/bao/uploaded/https://img.al…51059038/O1CN01MV6J3g2GdSZ4qiOyn_!!2251059038.png",
-        info:"2024中超联赛天津津门虎主场赛事（年票）",
-        position:"天津泰达足球场",
-        price:"680"
+        img: "https://img.alicdn.com/bao/uploaded/https://img.al…51059038/O1CN01MV6J3g2GdSZ4qiOyn_!!2251059038.png",
+        info: "2024中超联赛天津津门虎主场赛事（年票）",
+        position: "天津泰达足球场",
+        price: "680"
       },
       {
-        img:"https://img.alicdn.com/bao/uploaded/i3/2251059038/O1CN01FT1QXb2GdSZujlBWG_!!2-item_pic.png",
-        info:"2023-2024WCBA 山西竹叶青女篮季后赛（套票）",
-        position:"山西体育中心体育馆",
-        price:"179"
+        img: "https://img.alicdn.com/bao/uploaded/i3/2251059038/O1CN01FT1QXb2GdSZujlBWG_!!2-item_pic.png",
+        info: "2023-2024WCBA 山西竹叶青女篮季后赛（套票）",
+        position: "山西体育中心体育馆",
+        price: "179"
       },
     ]
   },
@@ -145,14 +145,23 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
+    // 当用户上拉加载更多以后，如果用户进行了下拉刷新
+    // 需要将数据进行重置
 
+    // this.setData({
+    //   numList:[1,2,3]
+    // })
+    // 在下拉刷新后，loading效果有可能不会回弹回去
+    // if (this.data.numList.length === 3) {
+    //   wx.stopPullDownRefresh()
+    // }
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom() {
-
+    console.log("监听用户上拉加载");
   },
 
   /**
