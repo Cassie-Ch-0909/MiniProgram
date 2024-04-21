a622a2d8559743d27ce5eccd3d3a55bb1c0d8d1d
Cassie-Ch-0909 <cassie20190909@gmail.com>
2024-04-21 19:42:41 +0800

    完成了一下课堂作业

--- pages/home/home.js
+++ pages/home/home.js
@@ -103,7 +103,19 @@ Page({
         position: "山西体育中心体育馆",
         price: "179"
       },
-    ]
+    ],
+    option1: [
+      { text: '全部商品', value: 0 },
+      { text: '新款商品', value: 1 },
+      { text: '活动商品', value: 2 },
+    ],
+    option2: [
+      { text: '默认排序', value: 'a' },
+      { text: '好评排序', value: 'b' },
+      { text: '销量排序', value: 'c' },
+    ],
+    value1: 0,
+    value2: 'a',
   },
 
   /**
