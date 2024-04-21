46b39f75dd3d0459308543d3a5c7d53b0bbdc721
Cassie-Ch-0909 <cassie20190909@gmail.com>
2024-04-21 15:38:27 +0800

    search搜索页面UI布局完成

--- modules/goodModule/pages/search/search.js
+++ modules/goodModule/pages/search/search.js
@@ -5,7 +5,92 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    countryHotSearchList: [
+      {
+        id: 1,
+        name: "周杰伦",
+        status: "爆"
+      },
+      {
+        id: 2,
+        name: "周深",
+        status: "爆"
+      },
+      {
+        id: 3,
+        name: "时代少年团",
+        status: "热"
+      },
+      {
+        id: 4,
+        name: "凤凰传奇",
+        status: "热"
+      },
+      {
+        id: 5,
+        name: "张杰",
+        status: "热"
+      },
+      {
+        id: 6,
+        name: "薛之谦",
+        status: "热"
+      },
+      {
+        id: 7,
+        name: "林俊杰",
+        status: "热"
+      },
+      {
+        id: 8,
+        name: "邓紫棋",
+        status: "热"
+      },
+      {
+        id: 9,
+        name: "许嵩",
+        status: "热"
+      },
+      {
+        id: 10,
+        name: "陈奕迅",
+        status: "热"
+      },
+    ],
+    cityHotSearchList:[
+      {
+        id:1,
+        content:"王心凌 宁波"
+      },
+      {
+        id:2,
+        content:"周兴哲(Eric) 宁波"
+      },
+      {
+        id:3,
+        content:"张鹤伦·郎鹤炎相声专场"
+      },
+      {
+        id:4,
+        content:"话剧 《2：22》"
+      },
+      {
+        id:5,
+        content:"话剧 《春逝》"
+      },
+      {
+        id:6,
+        content:"陈佩斯父亲首次同台"
+      },
+      {
+        id:7,
+        content:"GIN 宁波"
+      },
+      {
+        id:8,
+        content:"大船文化·亲子音乐舞台剧《敦煌奇妙夜》"
+      },
+    ]
   },
 
   /**
